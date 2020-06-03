6fb530ea5857e3fc2de8036415db9f907c7f4f15
bayraqi <66225004+bayraqi@users.noreply.github.com>
2020-06-03 22:12:31 +0200

    Updated file for new script

--- scripts/HNG-02805.php
+++ scripts/HNG-02805.php
@@ -1,5 +1,9 @@
-<?php
+<?php 
 
-echo "Hello World, this is Tinashe Bandera with HNGi7 ID 02805 using PHP for stage 2 task";
+$name = 'Tinashe Bandera';
+$id = 'HNG-02805';
+$language = 'PHP';
+
+echo "Hello World, this is {$name} with HNGi7 ID {$id} using {$language} for stage 2 task. bayraqi@outlook.com";
 
 ?>
